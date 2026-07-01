fb11676cd74518c4324217695f7c58ea6e4fbc18
Henrique Mariano <h_bromiel@hotmail.com>
2026-07-01 12:56:55 -0300

    Primeiro projeto: Manifesto com f-string

---
+++ First_project.py
@@ -0,0 +1,13 @@
+# -------------------------------------------------------------------------
+# PROJETO 01
+# Prática: Variáveis, Métodos de String e Formatação com f-strings
+# -------------------------------------------------------------------------
+
+
+autor = "elon musk"
+autor_formatado = autor.title()
+
+frase = "Quando algo é importante o suficiente, você faz, mesmo que as probabilidades não estejam a seu favor."
+
+print(f'\n "{frase}"\n')
+print(f"\t- {autor_formatado} ")
