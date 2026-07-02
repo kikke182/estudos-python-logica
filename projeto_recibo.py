5e78e0737ec17983ddda1d18103eb0f9bb5287db
Henrique Mariano <h_bromiel@hotmail.com>
2026-07-01 21:52:21 -0300

    refactor: translate directories and files to English for international standards

---
+++ projeto_recibo.py
@@ -0,0 +1,14 @@
+# -------------------------------------------------------------------------
+# PROJECT 02
+# -------------------------------------------------------------------------
+client_name = "john"
+service_description = "Home remodeling at 367, labor only!"
+total_price = "500"
+
+receipt_details = (
+    f"Service: {service_description}\n"
+    f"Provided by: {client_name.title()}\n"
+    f"Total Amount: R${total_price}"
+)
+
+print(receipt_details)
